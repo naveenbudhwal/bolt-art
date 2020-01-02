c4a15297af19a7262aa21d78210580b87a08ad08
naveenbudhwal <naveenbudhwal@gmail.com>
2020-01-02 13:32:47 +0530

    Artwork detail page

---
+++ src/views/Detail.vue
@@ -0,0 +1,142 @@
+<template>
+  <div class="art-detail">
+    <div class="art">
+      <img :src=artwork.url alt="Artwork">
+    </div>
+    <div class="art-info">
+      <div class="art-name">{{artwork.name}}</div>
+      <div class="tag">
+        <div v-for="art_tag in artwork.tag" class="art-tag">{{art_tag}}</div>
+      </div>
+      <hr class="material-hr">
+      <div v-for="art_desc in artwork.desc" class="art-desc">{{art_desc}}</div>
+      <router-link to="/artworks">
+        <button class="back-btn">Go Back</button>
+    </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'art_detail',
+  data() {
+    return {
+      artwork: this.$store.getters.artwork(this.$route.params.id)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+.art-detail {
+  display: flex;
+  height: 85vh;
+}
+
+.art {
+  width: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.art img {
+  width: 350px;
+  border-radius: 10px;
+  transition: all 0.3s cubic-bezier(0.215, 0.610, 0.355, 1);
+}
+
+.art img:hover {
+  transform: scale(1.2);
+}
+
+.art-info {
+  width: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: flex-start; 
+  flex-direction: column;
+}
+
+.art-name {
+  font-size: 3em;
+  font-weight: 500;
+}
+
+.art-tag {
+  background: linear-gradient(to right, #fbb034, #ffdd00);
+  padding: 5px 10px;
+  border-radius: 15px;
+  color: #fff;
+  font-weight: 500;
+  align-self: flex-start;
+  margin: 5px 0;
+  margin: 0 5px;
+}
+
+.tag {
+  display: flex;
+  margin: 5px 0;
+}
+
+.art-desc {
+  padding-right: 15%;
+  font-weight: 400;
+  line-height: 1.3;
+}
+
+.material-hr {
+  width: 20%;
+  margin: 20px 0;
+  height: 5px;
+  background: #FFF9E8;
+  border-radius: 5px;
+  border: none;
+}
+
+.back-btn {
+  background: #2F4858;
+  color: #fff;
+  font-weight: 500;
+  border: none;
+  box-shadow: 0 3px 5px -1px rgba(0,0,0,.2), 0 6px 10px 0 rgba(0,0,0, .14), 0 1px 18px 0 rgba(0,0,0,.12);
+  padding: 10px 20px;
+  border-radius: 24px;
+  cursor: pointer;
+  margin: 30px 0;
+}
+
+/* -----------Responsive Media Queries------------ */
+
+@media only screen and (max-width: 576px) {
+  .art-detail {
+    flex-direction: column;
+  }
+  .art {
+    width: 100%;
+    margin: 50px 0;
+  }
+  .art img {
+    width: 250px;
+  }
+  .art-name {
+    padding: 0 5%;
+  }
+  .art-info {
+    width: 100%;
+    text-align: center;
+    align-items: center;
+  }
+  .art-desc {
+    padding: 0 10%;
+  }
+  .art-tag {
+    align-self: center;
+  }
+  .material-hr {
+    width: 30%;
+  }
+}
+</style>
